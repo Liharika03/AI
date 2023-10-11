d9bfa4c7a27da92affc962ce725106bffd7bd93d
Liharika03 <147531439+Liharika03@users.noreply.github.com>
2023-10-11 15:27:13 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,37 @@
+#bfs
+graph={
+    'p':['q','r','s'],
+    'q':['p','r'],
+    'r':['p','q','t'],
+    't':['r'],
+    's':['p']
+}
+visited=[]
+queue=[]
+def bfs(visited,graph,node):
+    visited.append(node)
+    queue.append(node)
+    while queue:
+        m=queue.pop(0)
+        print(m,end=" ")
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+print("following is the breadth first search")
+bfs(visited,graph,'p')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
